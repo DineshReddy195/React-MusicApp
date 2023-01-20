8c44745b8eacd18a84466abd0af39c3385996f8f
Dinesh Reddy <ndkr2716@gmail.com>
2023-01-20 08:07:38 +0530

    Assignment Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Music from "./Component/Music";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [indexOf, setIndexOf] = useState(0);
+  const backword = () => {
+    setIndexOf(indexOf - 1);
+    console.log(setIndexOf);
+  };
+
+  const [musicData, setMusicData] = useState([]);
+  const getMusicData = async () => {
+    const musicApi = await fetch(
+      `https://api.napster.com/v2.1/tracks/top?apikey=ZWE4NjZlYmEtMTlkNi00NDQwLWE5ZDgtMWNhOTE2NDYyNjFk`
+    );
+    console.log(musicApi);
+
+    const response = await musicApi.json();
+    console.log(response);
+
+    const aa = Object.values(response.tracks);
+   
+
+    setMusicData(aa);
+  };
+
+  useEffect(() => {
+    getMusicData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Music App</h1>
+      {musicData.map((item, index) => {
+        console.log(item.name);
+
+        return <Music value={item} key={index} />;
+      })}
+      <div className="bottom">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUS0UKYJuZbUrCmJM1ZShxe_tM2IW6KCvE7w&usqp=CAU"
+          width="50px"
+          onClick={backword}
+          alt="image"
+        />
+        <img
+          src="https://uxwing.com/wp-content/themes/uxwing/download/controller-and-music/pause-button-icon.png"
+          width="50px" alt="pic"
+        />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxuIddyxdEvbBanjxJT2T-Km7sdt6QYMy6sg&usqp=CAUhttps://png.pngtree.com/element_our/20190528/ourmid/pngtree-fast-forward-icon-image_1128381.jpg"
+          width="50px" alt="img"
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
